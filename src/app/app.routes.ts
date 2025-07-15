c9a2ef4f30a45d883cb6cef8a48c89b3770b738b
fae101 <faegobran@gmail.com>
2025-07-15 04:50:07 +0300

    register and login placement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,10 @@ import { Register } from './modules/public/register/register/register';
 import { Login } from './modules/public/login/login';
 
 export const routes: Routes = [
-{path:'',redirectTo:'teacher/create-exam',pathMatch:'full'},
 
+{path: '', component: Home },
 {path:'teacher/create-exam',component:CreateExam},
 {path:'register', component:Register},
-{ path: '', component: Home },
 {path:'login',component:Login}
 
 ];
